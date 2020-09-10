01c6ecc1b298a47ce2c58c585a7466add797032e
Rob Foulkrod <rob@foulkrodTraining.com>
2020-09-10 12:00:05 -0400

    Lab 1 - Nothing to see here

--- Lab1/Ex2/Program.cs
+++ Lab1/Ex2/Program.cs
@@ -6,14 +6,14 @@ namespace Ex2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Exercise 2 - Celsius to Farenheit converter");
+            Console.WriteLine("Exercise 2 - Celsius to fahrenheit converter");
 
             Console.Write("Enter a temperature in celsius: ");
             string userCelsius = Console.ReadLine();
             double celsius = double.Parse(userCelsius);
 
-            double farenheit = celsius * (9.0 / 5.0) + 32;
-            Console.WriteLine($"Your celsius of {celsius} converts to a farenhiet value of {farenheit}");
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            Console.WriteLine($"Your celsius of {celsius} converts to a fahrenheit value of {fahrenheit}");
         }
     }
 }
